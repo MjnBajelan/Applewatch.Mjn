d47e538b67e2cf74c026d52ede54266c906c7417
mjnbajelan <marjanbajelan7@gmail.com>
2024-02-08 19:53:33 +0330

    Apple watch

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+let smenusearch = document
+  .querySelector("div#smenusearch")
+  .addEventListener("click", function () {
+    alert("mmmmmmmmmmm");
+  });
+/*let smenushop = document
+  .querySelector("div#smenushop")
+  .addEventListener("click", function () {
+    alert("pppp");
+  });
+let flag = true;
+$("div.smenu>nav>ul>li:nth-of-type(5)").on("click", function () {
+  if (flag) {
+    $("div.smenu>nav>ul>li:nth-of-type(5)>div:nth-of-type(1)").css({
+      transform: "rotate(45deg)",
+      transition: "60ms",
+      top: "calc(50% - 1px)",
+    }),
+      $("div.smenu>nav>ul>li:nth-of-type(5)>div:nth-of-type(2)").css({
+        transform: "rotate(-45deg)",
+        transition: "60ms",
+        top: "calc(50% - 1px)",
+      });
+    $("div.smenu>nav>ul>li:nth-of-type(13)>a>div.search").css({
+      top: "-350",
+      transition: "500ms",
+    });
+    flag = false;
+  } else {
+    $("div.smenu>nav>ul>li:nth-of-type(5)>div:nth-of-type(1)").css({
+      transform: "rotate(0)",
+      transition: "60ms",
+      top: "18px",
+    }),
+      $("div.smenu>nav>ul>li:nth-of-type(5)>div:nth-of-type(2)").css({
+        transform: "rotate(0)",
+        transition: "60ms",
+        top: "25px",
+      });
+    flag = true;
+  }
+});
+*/
